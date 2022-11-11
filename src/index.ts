28d4314faff780753d0b486c50fbd0f711389527
LeBogo <lebogomc@gmail.com>
2022-11-11 08:59:42 +0100

    Added events to exports

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,15 @@ export * from "./GameMode";
 export * from "./OnuSettings";
 export * from "./PlayerlistPlayer";
 export * from "./UUIDGenerator";
+
+export * from "./events/CardPlacedEvent";
+export * from "./events/CardRequestEvent";
+export * from "./events/DisconnectedEvent";
+export * from "./events/GameStartEvent";
+export * from "./events/JoinedLobbyEvent";
+export * from "./events/JoinLobbyEvent";
+export * from "./events/ReconnectedEvent";
+export * from "./events/ReconnectEvent";
+export * from "./events/SettingsChangedEvent";
+export * from "./events/UpdateDeckEvent";
+export * from "./events/UpdatePlayerlistEvent";
